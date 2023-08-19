47bde6a87ad1fc8407b4f6d1660762b35600273f
Luan <luan.carlos.3174@gmail.com>
2023-08-19 00:58:22 -0300

    feat: creates list of entities to use in api

---
+++ src/entities/gpt.entity.go
@@ -0,0 +1,38 @@
+package entities
+
+type GptQuery struct {
+	Prompt string
+}
+
+type GptRequest struct {
+	Model       string    `json:"model"`
+	Messages    []message `json:"messages"`
+	Temperature float64   `json:"temperature"`
+	N           int64     `json:"n"`
+}
+
+type GptResponse struct {
+	Id      string   `json:"id"`
+	Object  string   `json:"object"`
+	Created int64    `json:"created"`
+	Model   string   `json:"model"`
+	Choices []choice `json:"choices"`
+	Usage   usage    `json:"usage"`
+}
+
+type choice struct {
+	Index   int64   `json:"index"`
+	Message message `json:"message"`
+}
+
+type message struct {
+	Role          string `json:"role"`
+	Content       string `json:"content"`
+	Finish_reason string `json:"finish_reason"`
+}
+
+type usage struct {
+	Prompt_tokens     int64 `json:"prompt_tokens"`
+	Completion_tokens int64 `json:"completion_tokens"`
+	Total_tokens      int64 `json:"total_tokens"`
+}
